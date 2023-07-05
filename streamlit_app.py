70b640c7b3fa4e8e188b20ba80592f10f10f6b4c
JimSteele44 <138675580+JimSteele44@users.noreply.github.com>
2023-07-05 12:24:02 +0100

    Update streamlit_app.py
    Icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.write('Omega 3 & Blueberry Oatmeal')
-streamlit.write('Kale, Spinach & Rocket Smoothie')
-streamlit.write('Hard-Bolied Free-Range Egg')
+streamlit.write('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.write('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.write('🐔 Hard-Bolied Free-Range Egg')
+streamlit.write('🥑🍞 Avacado Toast')
