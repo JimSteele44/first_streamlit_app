a81cb5397d146d4a2d681a24b4bbbf0e16526483
JimSteele44 <138675580+JimSteele44@users.noreply.github.com>
2023-07-05 13:32:02 +0100

    Update streamlit_app.py
    Selected items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ streamlit.write('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.write('🐔 Hard-Bolied Free-Range Egg')
 streamlit.write('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+fruits_selected = streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
